8f04775d1e391496be27ecb63c1a8b09b75b627b
Mike Anglebrandt <manglebrandt@epfc.com>
2022-12-16 14:50:00 -0500

    fdgfdg

--- src/app.js
+++ src/app.js
@@ -8,3 +8,9 @@ app.set('view engine', 'ejs');
 app.set('views', express.static(path.join(__dirname, 'views')));
 
 app.set(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('title:', 'Index')
+});
+
+app.listen(3000, "PS Project Running on port 3000!");
