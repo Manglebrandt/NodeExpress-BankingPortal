c4bb9bdc780fc3133157762c15f3a97ab9ec0234
Mike Anglebrandt <manglebrandt@epfc.com>
2022-12-16 13:58:37 -0500

    bgf

--- src/app.js
+++ src/app.js
@@ -2,7 +2,9 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-const app = express;
+const app = express();
 
+app.set('view engine', 'ejs');
 app.set('views', express.static(path.join(__dirname, 'views')));
-//app.set('view engine', 'ejs');
+
+app.set(express.static('public'));
